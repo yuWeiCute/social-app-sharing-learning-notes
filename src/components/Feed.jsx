6fd3998b1449b7cec527c42190d5c95af71b5bf6
yuweiCute <jiang_yuwei@foxmail.com>
2022-02-27 16:22:17 +0800

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+//with this you can find which category been looking at
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
+
+const Feed = () => {
+  const [loading, setLoading] = useState(false);
+
+  const [pins, setPins] = useState();
+
+  const { categoryId } = useParams();
+
+
+  useEffect(() => {
+    if (categoryId) {
+      setLoading(true);
+      const query = searchQuery(categoryId);
+      client.fetch(query).then((data) => {
+        setPins(data);
+        console.log(data);
+        setLoading(false);
+      });
+    } else {
+      setLoading(true);
+
+      client.fetch(feedQuery).then((data) => {
+        setPins(data);
+        console.log(data);
+        setLoading(false);
+      });
+    }
+  }, [categoryId]);
+
+  const ideaName = categoryId || 'new';
+  if (loading) {
+    return (
+      <Spinner message={`We are adding ${ideaName} ideas to your feed!`} />
+    );
+  }
+  return (
+    <div>
+      {pins && (
+        <MasonryLayout pins={pins} />
+      )}
+    </div>
+  );
+};
+
+export default Feed;
