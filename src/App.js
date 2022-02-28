a173f82d4de06a196148a8fb22d26969f6b7f3b8
yuweiCute <jiang_yuwei@foxmail.com>
2022-03-01 02:32:32 +0800

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import { Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Home from './container/Home';
-import { UserProfile } from './components';
+import React, { Fragment, Suspense, lazy } from 'react'
+import { Route, Routes } from 'react-router-dom';
+
+const Login = lazy(() => import("./components/Login"));
+const Home = lazy(() => import("./container/Home"));
+const UserProfile = lazy(() => import("./components/UserProfile"));
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="/*" element={<Home />} />
-      <Route path="/user-profile/:userId" element={<UserProfile />} />
-    </Routes>
+
+    <Suspense fallback={<Fragment />}>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user-profile/:userId" element={<UserProfile />} />
+      </Routes>
+    </Suspense>
+
+
   )
 }
 
